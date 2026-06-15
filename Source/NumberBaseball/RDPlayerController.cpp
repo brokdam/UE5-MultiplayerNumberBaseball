6dfe2e4f2de34969ee626a235e7a537514f9da36
brogdam <brogdam208@kakao.com>
2026-06-15 12:31:14 +0900

    Server RPC로 클라이언트 추측을 서버에 전송
    ServerSubmitGuess(Server RPC)로 클라이언트 입력을 서버에 보내고 서버의 GameMode가 ProcessGuess로 판정하도록 연결함
    
    입력은 임시 키 바인딩(Enter)으로 테스트
    (UMG 도입 시 교체 예정)

--- Source/NumberBaseball/RDPlayerController.cpp
+++ Source/NumberBaseball/RDPlayerController.cpp
@@ -1,5 +1,26 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "RDPlayerController.h"
+#include "RDGameMode.h"
 
+void ARDPlayerController::ServerSubmitGuess_Implementation(const FString& Guess)
+{
+	UE_LOG(LogTemp, Warning, TEXT("[Server] ServerSubmitGuess running, guess: %s"), *Guess);
+	
+	ARDGameMode* GM = GetWorld()->GetAuthGameMode<ARDGameMode>();
+	if (GM)
+	{
+		GM->ProcessGuess(Guess);
+	}
+}
+
+void ARDPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	InputComponent->BindKey(EKeys::Enter, IE_Pressed, this, &ARDPlayerController::OnTestSubmit);
+
+}
+
+void ARDPlayerController::OnTestSubmit()
+{
+	UE_LOG(LogTemp, Warning, TEXT("[Client] OnTestSubmit -> calling ServerSubmitGuess"));
+	ServerSubmitGuess(TEXT("429"));
+}
--- Source/NumberBaseball/RDPlayerController.h
+++ Source/NumberBaseball/RDPlayerController.h
@@ -1,17 +1,19 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "RDPlayerController.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class NUMBERBASEBALL_API ARDPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public: 
+	UFUNCTION(Server, Reliable)
+	void ServerSubmitGuess(const FString& Guess);
+	
+protected:
+	virtual void SetupInputComponent() override;
+	void OnTestSubmit();
 };
